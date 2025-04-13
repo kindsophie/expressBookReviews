1bb49b068237dc8a945925d6ea57b8faebb0f86f
kindsophie <ingridsophiekind@gmail.com>
2025-04-13 09:39:22 -0400

    making functions async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,7 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
       try {
         const response = await axios.get('URL_TO_GET_BOOKS');
         const books = response.data;
@@ -30,7 +30,7 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
     try {
         const response = await axios.get(`URL_TO_GET_BOOK_BY_ISBN/${isbn}`); // Replace with the actual URL or API endpoint
@@ -42,7 +42,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
       const author = req.params.author;
     try {
         const response = await axios.get(`URL_TO_GET_BOOKS_BY_AUTHOR/${author}`);
@@ -54,7 +54,7 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
       const title = req.params.title;
     try {
         const response = await axios.get(`URL_TO_GET_BOOKS_BY_TITLE/${title}`);
